f06ddc6079b11d4ee63b3e134f7860be723de719
farazmsiddiqi <farazs27@gmail.com>
2022-02-12 14:52:18 -0600

    add softmax, svm

--- CS444_assignment1/models/softmax.py
+++ CS444_assignment1/models/softmax.py
@@ -13,7 +13,7 @@ class Softmax:
             epochs: the number of epochs to train for
             reg_const: the regularization constant
         """
-        self.w = None  # TODO: change this
+        self.W = None  # TODO: change this
         self.lr = lr
         self.epochs = epochs
         self.reg_const = reg_const
@@ -34,8 +34,25 @@ class Softmax:
         Returns:
             gradient with respect to weights w; an array of same shape as w
         """
-        # TODO: implement me
-        return
+        # TODO: implemented
+        N = X_train.shape[0]
+        grad_w = np.zeros((self.n_class, X_train.shape[1]))
+
+        for obs in range(N):
+            # Scores for all classes
+            scores = self.W @ X_train[obs]
+            # computing softmax "probabilities"
+            log_k = -np.max(scores)
+            softmax_denom = np.sum(np.exp(scores + log_k))
+            softmax_probabilities = np.exp(scores + log_k) / softmax_denom
+
+            # compute gradient for each observation and store vales according to update rule from slides
+            for c in range(self.n_class):
+                if c == y_train[obs]:
+                    grad_w[c] += (softmax_probabilities[c] - 1) * X_train[obs]
+                elif c != y_train[obs]:
+                    grad_w[c] += softmax_probabilities[c] * X_train[obs]
+        return grad_w + self.W * self.reg_const / N
 
     def train(self, X_train: np.ndarray, y_train: np.ndarray):
         """Train the classifier.
@@ -47,8 +64,16 @@ class Softmax:
                 N examples with D dimensions
             y_train: a numpy array of shape (N,) containing training labels
         """
-        # TODO: implement me
-        return
+        N = X_train.shape[0]
+
+        self.W = np.random.rand(self.n_class, X_train.shape[1])  # Initializing the weights randomly
+        mini_batch_size = min((250, N))  # in case we have fewer than 250 training points
+
+        for epoch in range(self.epochs):
+            batch_idx = np.random.choice(N, mini_batch_size)  # choose <batch_size> i.i.d. samples
+            # Linearly decaying learning rate as number of epochs increases
+            decayed_lr = self.lr * (1 - epoch / self.epochs)
+            self.W -= decayed_lr * self.calc_gradient(X_train[batch_idx], y_train[batch_idx])
 
     def predict(self, X_test: np.ndarray) -> np.ndarray:
         """Use the trained weights to predict labels for test data points.
@@ -62,5 +87,9 @@ class Softmax:
                 length N, where each element is an integer giving the predicted
                 class.
         """
-        # TODO: implement me
-        return
+        # TODO: implemented
+        final_predictions = []
+        for dp in range(X_test.shape[0]):
+            final_predictions.append(np.argmax(self.W @ X_test[dp]))
+
+        return final_predictions
